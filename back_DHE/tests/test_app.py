3d4f3f7dc424c08029d837ef9fd5aad92f8b5c8c
luiz filipe <luizfilipelgs@gmail.com>
2025-05-28 12:18:33 -0300

    chore: testing schemas with pydantic

--- back_DHE/tests/test_app.py
+++ back_DHE/tests/test_app.py
@@ -12,3 +12,10 @@ def test_hello_world():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello World!!!'}
+
+
+def test_hello_world_pag():
+    response = client.get('/olamundo')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1>Hello World!!!</h1>' in response.text
